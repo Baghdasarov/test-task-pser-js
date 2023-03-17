ee163c4f2a518801e28d51122a9d295794adac48
Norayr Baghdasarov <noro9424@gmail.com>
2023-03-17 14:24:35 +0400

    #last optimizations

--- src/api/operations.api.mjs
+++
@@ -1,7 +0,0 @@
-const OPERATIONS_API_URL = 'https://developers.paysera.com/tasks/api'
-
-export const OPERATIONS_API = {
-  cacheInConfig: `${OPERATIONS_API_URL}/cash-in`,
-  cacheOutNaturalConfig: `${OPERATIONS_API_URL}/cash-out-natural`,
-  cacheOutJuridicalConfig: `${OPERATIONS_API_URL}/cash-out-juridical`,
-}
